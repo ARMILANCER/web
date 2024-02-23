e7978dc229c042004d8752fc15815ad63f2fe986
ARMILANCER <114140097+ARMILANCER@users.noreply.github.com>
2024-02-23 11:51:00 +0100

    Add files via upload

---
+++ Mail/html/js/home.js
@@ -0,0 +1,7 @@
+var mail = document.getElementById('mail').value;
+var password = document.getElementById('password').value;
+
+if(mail != null && password != null){
+    alert (mail);
+    alert (password); 
+} 
