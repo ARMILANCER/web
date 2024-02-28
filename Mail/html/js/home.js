985ef5335abb0da7198f609c090528f404d5cbba
ARMILANCER <114140097+ARMILANCER@users.noreply.github.com>
2024-02-28 11:44:12 +0100

    Delete Mail/html/js/home.js

--- Mail/html/js/home.js
+++
@@ -1,7 +0,0 @@
-var mail = document.getElementById('mail').value;
-var password = document.getElementById('password').value;
-
-if(mail != null && password != null){
-    alert (mail);
-    alert (password); 
-} 
