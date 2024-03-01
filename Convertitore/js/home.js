01e6a3b94da8cccb9407550a0e3603ceb316b6de
ARMILANCER <114140097+ARMILANCER@users.noreply.github.com>
2024-03-01 05:14:32 +0100

    Update home.js

--- Convertitore/js/home.js
+++ Convertitore/js/home.js
@@ -1,25 +1,25 @@
-console.log("yes");
-let valChange = document.getElementById("value_change");
-let valBase = document.getElementById("value_base");
-let textA_base = document.getElementById("textA_base");
-let textA_Change = document.getElementById("textA_change");
+var valChange = document.getElementById("value_change");
+var valBase = document.getElementById("value_base");
+var textA_base = document.getElementById("textA_base");
+var textA_change = document.getElementById("textA_change");
 
 textA_base.addEventListener("input",function(){
-    console.log(valBase.value);
-    console.log(importo(valChange.value, parseFloat(textA_base.value)));
-    textA_Change.value = importo(valChange.value, parseFloat(textA_base.value));
+    // console.log(valBase.value);
+    // console.log(importo(valChange.value, parseFloat(textA_base.value)));
+    this.value = this.value.replace(/[^\d.]/g, '');
+    textA_change.value = importo(valChange.value, parseFloat(textA_base.value));
 });
-
-textA_Change.addEventListener("input",function(){
-    console.log(valChange.value);
-    console.log(importo(valBase.value, parseFloat(textA_Change.value)));
-    textA_base.value = importo(valBase.value, parseFloat(textA_Change.value));
+textA_change.addEventListener("input",function(){
+    // console.log(valChange.value);
+    // console.log(importo(valBase.value, parseFloat(textA_change.value)));
+    this.value = this.value.replace(/[^\d.]/g, '');
+    textA_base.value = importo(valBase.value, parseFloat(textA_change.value));
 });
-function importo(value, importo){ 
+function importo(value, importo){
     let res = value * importo
     if(/[0-9]/.test(res)){
         if(!Number.isInteger(res)){
-            return (value * importo).toFixed(3);
+            return (value * importo).toFixed(4);
         }
         return value * importo;
     }
